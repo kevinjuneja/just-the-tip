cff2cc3bd598ef024e9b2143591e7519f44bde1b
Kevin Juneja <kevin.j.24@gmail.com>
2013-06-29 12:32:44 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Kevin Juneja on 6/29/13.
+//  Copyright (c) 2013 Mod Squad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TipCalculatorTests/TipCalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  TipCalculatorTests.h
+//  TipCalculatorTests
+//
+//  Created by Kevin Juneja on 6/29/13.
+//  Copyright (c) 2013 Mod Squad. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TipCalculatorTests : SenTestCase
+
+@end
